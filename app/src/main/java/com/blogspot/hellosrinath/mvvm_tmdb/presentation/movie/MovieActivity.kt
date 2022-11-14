db33d947fc1801632dbe4b83ea437548d5a807ec
SRI NATH <s.nath32@yahoo.com>
2022-11-14 17:42:30 +0600

    calling movie data and test di

--- app/src/main/java/com/blogspot/hellosrinath/mvvm_tmdb/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/blogspot/hellosrinath/mvvm_tmdb/presentation/movie/MovieActivity.kt
@@ -2,11 +2,20 @@ package com.blogspot.hellosrinath.mvvm_tmdb.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.blogspot.hellosrinath.mvvm_tmdb.R
 import com.blogspot.hellosrinath.mvvm_tmdb.databinding.ActivityMovieBinding
+import com.blogspot.hellosrinath.mvvm_tmdb.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: MovieViewModelFactory
+    private lateinit var movieViewModel: MovieViewModel
+
     private lateinit var binding: ActivityMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +23,18 @@ class MovieActivity : AppCompatActivity() {
             this,
             R.layout.activity_movie
         )
+
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
+        movieViewModel = ViewModelProvider(this, factory)
+            .get(MovieViewModel::class.java)
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer {
+            Log.i(Companion.TAG, "onCreate: ${it.toString()}")
+        })
+    }
+
+    companion object {
+        private const val TAG = "MovieActivity"
     }
 }
